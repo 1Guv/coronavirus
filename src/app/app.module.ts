fba4ff7c52e8e0081f1075f162ccd091f1de7644
Guv <Guv@thehouseshop.com>
2020-04-13 14:21:44 +0100

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LiveChartComponent } from './live-chart/live-chart.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './_material/all-angular-material-modules';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { LiveChartComponent } from './live-chart/live-chart.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
