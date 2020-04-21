99297d7368bd972cb4dc853f9adaa1abdce024f9
Guv <Guv@thehouseshop.com>
2020-04-21 20:58:17 +0100

    refactored and deleted code

--- src/app/live-chart/live-chart.component.ts
+++ src/app/live-chart/live-chart.component.ts
@@ -3,10 +3,6 @@ import { Covid191Service } from '../_services/covid19-1.service';
 import { Covid19DataClass, TimeLineClass, HistoricalDataClass } from '../_models/covid19';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Covid19UKDataClass, UKCountriesClass } from '../_models/covid19UK';
-import { Observable, of } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import { Covid19UKDataService } from '../_services/covid19-uk-data.service';
 
 @Component({
   selector: 'app-live-chart',
@@ -21,39 +17,9 @@ export class LiveChartComponent implements OnInit, OnChanges {
   countryTimeline: TimeLineClass;
   countryHistory: TimeLineClass;
   counto: any;
-  // country: string;
-  countriesInUK$;
-  regionsInUK$: Observable<any[]>;
-  covid19UKData$: Observable<any>;
-
-  arrayOfUKCountries = []
-
-  uk = {
-    england: {
-      totalCases: 0,
-      deaths: 0,
-      dailyDeaths: [],
-      dailyTotalDeaths: [],
-      dailyConfirmedCases: [],
-      dailyTotalConfirmedCases: []
-    },
-
-    scotland: {
-      totalCases: 0,
-      deaths: 0,
-      dailyDeaths: [],
-      dailyTotalDeaths: [],
-      dailyConfirmedCases: [],
-      dailyTotalConfirmedCases: []
-    },
-
-    // wales: UKCountriesClass;
-    // ni: UKCountriesClass;
-  }
 
   constructor(
     private covid19: Covid191Service,
-    private covid19UKData: Covid19UKDataService,
     private snackBar: MatSnackBar) { }
 
   ngOnInit() {
